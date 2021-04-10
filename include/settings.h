e87abf9389b116024f07d0d8be57b748316e2f10
Mitchell Ton <mitchell@vention.nl>
2021-04-11 00:02:33 +0200

    Safe settings file

---
+++ include/settings.h
@@ -0,0 +1,19 @@
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+#define HUEHUB_IP "192.168.1.1"
+#define HUEHUB_APIKEY "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+#define HUEHUB_REMOTEID 2
+
+#define NANOLEAF_ONE_IP "192.168.1.1"
+#define NANOLEAF_ONE_APIKEY "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+#define NANOLEAF_ONE_DEFAULT_EFFECT "Default"
+
+#define NANOLEAF_TWO_ENABLED false
+#define NANOLEAF_TWO_IP "192.168.1.1"
+#define NANOLEAF_TWO_APIKEY "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+#define NANOLEAF_TWO_DEFAULT_EFFECT "Default"
+
+#define POLL_SPEED 2000
+
+#endif /* SETTINGS_H */
